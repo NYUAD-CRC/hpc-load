524c59fef9cbee4cb98e99a7c3888dbbce4d3f2f
Waqqas Zia <zia.waqqas@nyu.edu>
2022-11-09 15:25:45 +0400

    Wed Nov  9 15:25:45 +04 2022

--- Software_tracker/Parser_Dalma.py
+++ Software_tracker/Parser_Dalma.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import plotly
 import plotly.express as px
-
+import plotly.io as pio
 df = pd.read_csv('Dalma_Results.csv')
 
 fig = px.bar(df, x = 'Application', y = 'Cpus', title='Application utilization')
 #fig.show()
-plotly.offline.plot(fig, filename='./Dalma_plot.html')
+#plotly.offline.plot(fig, filename='./Dalma_plot.html')
+
+pio.write_html(fig,'Dalma_plot.html')
