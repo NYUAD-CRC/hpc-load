eb77b90372de982b94ae1fc910a518b1a5a3a4f0
Waqqas Zia <zia.waqqas@nyu.edu>
2022-11-10 16:46:43 +0400

    Thu Nov 10 16:46:43 +04 2022

--- Software_tracker/accumulator.sh
+++ Software_tracker/accumulator.sh
@@ -2,6 +2,8 @@
 
 rm Application.csv
 rm Apps.csv
+rm Applications_dalma
+rm Applications_jubail
 
 cat Dalma_Results.csv | cut -d "," -f 1 |tail -n+2 >Application.csv
 cat Jubail_Results.csv | cut -d "," -f 1 |tail -n+2 >>Application.csv
@@ -11,19 +13,20 @@ file=$(cat Apps.csv)
 
 for line in $file
 do	
-echo -e "$line"
+#echo -e "$line"
 if grep "$line" Dalma_Results.csv
     then
             count=$(grep $line Dalma_Results.csv | cut -d "," -f 2)
     else
             count="0"
     fi
-    echo "Count for $line is $count"
+    echo "$count" >>Applications_dalma
+    if grep "$line" Jubail_Results.csv
+    then
+            count=$(grep $line Jubail_Results.csv | cut -d "," -f 2)
+    else
+            count="0"
+    fi
+    echo "$count" >>Applications_jubail
+
 done
-#     if grep -Fxq "$line" 
- #   then
-#	    count=$(grep $line Dalma_Results.csv | cut -d ":" -f 2)
- #   else
-  #  count=0
-   # fi
-#echo "Count for $line is $count"
