cf5f2bce71a7bb08ae2e1ca32d1c78a733ccd9a4
Waqqas Zia <zia.waqqas@nyu.edu>
2022-11-10 17:07:24 +0400

    Thu Nov 10 17:07:24 +04 2022

--- Software_tracker/final_chart.py
+++ Software_tracker/final_chart.py
@@ -1,4 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 apps=np.loadtxt("Apps.csv",dtype="str")
 print(apps)
+
+dalma=np.loadtxt("dalma_apps_final",dtype="int")
+print(dalma)
+
+jubail=np.loadtxt("jubail_apps_final",dtype="int")
+print(jubail)
+
+# plot bars in stack manner
+plt.bar(apps, dalma, color='r')
+plt.bar(apps, jubail, bottom=dalma, color='b')
+plt.xlabel("Application")
+plt.ylabel("Processes")
+plt.legend(["Dalma", "Jubail"])
+plt.title("Application utilization")
+plt.show()
