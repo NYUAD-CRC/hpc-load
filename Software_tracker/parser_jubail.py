5ac32dfa85159ee6463d6e44711556b523d12944
Waqqas Zia <zia.waqqas@nyu.edu>
2022-11-10 09:42:06 +0400

    Thu Nov 10 09:42:06 +04 2022

--- Software_tracker/parser_jubail.py
+++ Software_tracker/parser_jubail.py
@@ -8,5 +8,5 @@ df = pd.read_csv('Jubail_Results.csv')
 fig = px.bar(df, x = 'Application', y = 'Processes', title='Application utilization')
 #fig.show()
 #plotly.offline.plot(fig, filename='./Jubail_plot.html')
-pio.write_html(fig,'jubail_plot_2.html')
+pio.write_html(fig,'jubail_plot.html')
 
