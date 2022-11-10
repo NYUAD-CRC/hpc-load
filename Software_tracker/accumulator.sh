9b2da568f66885f2201d2dc75d8bb50d48e69d7e
Waqqas Zia <zia.waqqas@nyu.edu>
2022-11-10 16:20:49 +0400

    Thu Nov 10 16:20:49 +04 2022

--- Software_tracker/accumulator.sh
+++ Software_tracker/accumulator.sh
@@ -12,9 +12,16 @@ file=$(cat Apps.csv)
 for line in $file
 do
 	echo -e "$line"
+	     if grep -Fxq "$line"
+    then
+            count=$(grep $line Dalma_Results.csv | cut -d ":" -f 2)
+    else
+    count=0
+    fi
+    echo "Count for $line is $count"
 done
-    # if grep -Fxq "$line" 
-    #then
+#     if grep -Fxq "$line" 
+ #   then
 #	    count=$(grep $line Dalma_Results.csv | cut -d ":" -f 2)
  #   else
   #  count=0
